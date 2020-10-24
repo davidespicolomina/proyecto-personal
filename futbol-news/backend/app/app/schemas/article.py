54c5fc2d3d4fcf858eb815151449dd9ce8753dcd
David Espi Colomina <davidespicolomina@gmail.com>
2020-10-24 11:39:15 +0200

    Pydantic schema y crud para Article
    Estructuras de datos para devolver la información en la API y código para crear y modificar nuevos artículos.

---
+++ futbol-news/backend/app/app/schemas/article.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+# Shared properties
+class ArticleBase(BaseModel):
+    source: str
+    title: str
+    url: str
+    description: Optional[str] = None
+    favorite: bool = True
+    summary: str
+
+
+# Properties to receive on Article creation
+class ArticleCreate(ArticleBase):
+    pass
+
+
+# Properties to receive on Article update
+class ArticleUpdate(ArticleBase):
+    pass
+
+
+# Properties shared by models stored in DB
+class ArticleInDBBase(ArticleBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+# Properties to return to client
+class Article(ArticleInDBBase):
+    pass
+
+
+# Properties properties stored in DB
+class ArticleInDB(ArticleInDBBase):
+    pass
