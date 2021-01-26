807445546d493f9c092720e18e5fefa3281da4c9
David Espi Colomina <davidespicolomina@gmail.com>
2021-01-26 20:35:02 +0100

    Artículos funcionando

--- frontend/futnews/frontend/src/components/articles/ArticleFilter.js
+++ frontend/futnews/frontend/src/components/articles/ArticleFilter.js
@@ -9,9 +9,9 @@ class ArticleFilter extends Component {
         super(props);
         this.state = {
             filter: "",
-            page: props.data.page,
+            currentPage: props.data.page,
         };
-        this.totalPages = Math.ceil(props.total / props.size);
+        this.totalPages = Math.ceil(props.data.total / props.data.size);
     }
 
     handleChange = (event) => {
@@ -19,32 +19,65 @@ class ArticleFilter extends Component {
     }
 
     handleFilter = async (event) => {
-        await this.props.retrieveArticles(this.state.filter);
+        await this.props.retrieveArticles(0, this.state.filter);
+    }
+
+    handleClickPrevious = async (event) => {
+        await this.props.retrieveArticles(this.state.currentPage - 1, this.state.filter);
+        this.setState({currentPage: this.state.currentPage - 1});
+    }
+
+    handleClickNext = async (event) => {
+        await this.props.retrieveArticles(this.state.currentPage + 1, this.state.filter);
+        this.setState({currentPage: this.state.currentPage + 1});
+    }
+
+    renderPreviousLink = () => {
+        const currentPage = this.state.currentPage;
+        if(currentPage <= 0) {
+            return null;
+        }
+        return <p className="level-item" onClick={this.handleClickPrevious}><a>Previa</a></p>
+    }
+
+    renderNextLink = () => {
+        const currentPage = this.state.currentPage;
+        if(currentPage === this.totalPages - 1) {
+            return null;
+        }
+        return <p className="level-item" onClick={this.handleClickNext}><a>Siguente</a></p>
     }
 
     renderFilterForm = () => {
         return (
-            <div className="field is-horizontal">
-                <div className="field-body">
-                    <div className="field-label is-normal">
-                        <label className="label">{this.props.data.total} artículos</label>
-                    </div>
-                    <div className="field">
-                        <p className="control">
-                            <input className="input" type="text" placeholder="Filtrar título o contenido" onChange={this.handleChange} />
+            <nav className="level">
+                <div className="level-left">
+                    <div className="level-item">
+                        <p className="subtitle is-5">
+                            <strong>{this.props.data.total}</strong> artículos
                         </p>
                     </div>
-                    <div className="field-body">
-                        <div className="field">
-                            <div className="control">
-                                <button className="button is-primary" onClick={this.handleFilter}>
+                    <div className="level-item">
+                        <div className="field has-addons">
+                            <p className="control">
+                                <input className="input" type="text" placeholder="Filtrar" onChange={this.handleChange} />
+                            </p>
+                            <p className="control">
+                                <button className="button" onClick={this.handleFilter}>
                                     Filtrar
                                 </button>
-                            </div>
+                            </p>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div className="level-right">
+                    <p className="level-item">
+                        Página {this.state.currentPage + 1} de {Math.ceil(this.props.data.total / this.props.data.size)}.
+                    </p>
+                    {this.renderPreviousLink()}
+                    {this.renderNextLink()}
+                </div>
+            </nav>
         );
     }
 
